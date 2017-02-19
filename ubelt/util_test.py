e5bbba16a770999fffb4ed6bba38b534abbd8561
joncrall <crallj@rpi.edu>
2017-02-19 17:44:53 -0500

    wip

--- ubelt/util_test.py
+++ ubelt/util_test.py
@@ -367,8 +367,6 @@ def doctest_package(package_name=None, command=None, argv=None, verbose=None,
             cov_report = stream.read()
             print('Coverage Report:')
             print(cov_report)
-            import utool
-            utool.embed()
         return n_passed == n_total
         # TODO: test summary
 
