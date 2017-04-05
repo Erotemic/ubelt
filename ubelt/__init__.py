268c7708111389354ef73ff40208c54cc6a9071d
joncrall <crallj@rpi.edu>
2017-04-05 11:44:31 -0400

    version bump

--- ubelt/__init__.py
+++ ubelt/__init__.py
@@ -27,7 +27,7 @@ TODO:
 from __future__ import absolute_import, division, print_function, unicode_literals
 import sys
 
-__version__ = '0.0.15'
+__version__ = '0.0.16'
 
 GLOBAL_MODULES = [
     'util_const',
