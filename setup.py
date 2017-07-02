672277078311271331fd4e9b4e7672e9da58c7ec
joncrall <crallj@rpi.edu>
2017-07-02 17:14:41 -0400

    description for pypi

--- setup.py
+++ setup.py
@@ -77,22 +77,28 @@ def parse_description():
     print('readme_fpath = %r' % (readme_fpath,))
     # This breaks on pip install, so check that it exists.
     if exists(readme_fpath):
-        # strip out markdown to make a clean readme for pypi
-        textlines = []
-        with open(readme_fpath, 'r') as f:
-            capture = False
-            for line in f.readlines():
-                if '# Purpose' in line:
-                    capture = True
-                elif line.startswith('##'):
-                    break
-                elif capture:
-                    textlines += [line]
-        text = ''.join(textlines).strip()
-        text = text.replace('\n\n', '_NLHACK_')
-        text = text.replace('\n', ' ')
-        text = text.replace('_NLHACK_', '\n\n')
-        return text
+        try:
+            # convert markdown to rst for pypi
+            import pypandoc
+            return pypandoc.convert(readme_fpath, 'rst')
+        except Exception as ex:
+            # strip out markdown to make a clean readme for pypi
+            textlines = []
+            with open(readme_fpath, 'r') as f:
+                capture = False
+                for line in f.readlines():
+                    if '# Purpose' in line:
+                        capture = True
+                    elif line.startswith('##'):
+                        break
+                    elif capture:
+                        textlines += [line]
+            text = ''.join(textlines).strip()
+            text = text.replace('\n\n', '_NLHACK_')
+            text = text.replace('\n', ' ')
+            text = text.replace('_NLHACK_', '\n\n')
+            return text
+
 
 version = parse_version()
 
--- ubelt/__init__.py
+++ ubelt/__init__.py
@@ -26,7 +26,7 @@ TODO:
 from __future__ import absolute_import, division, print_function, unicode_literals
 import sys
 
-__version__ = '0.0.25'
+__version__ = '0.0.26'
 
 GLOBAL_MODULES = [
     'util_const',
