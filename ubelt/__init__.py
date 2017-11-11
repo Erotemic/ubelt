e49e9976bda9636b089d2cc57e23ee337cd3730e
Jon Crall <erotemic@gmail.com>
2017-11-11 17:46:26 -0500

    pre 0.0.34 commit

--- ubelt/__init__.py
+++ ubelt/__init__.py
@@ -26,7 +26,7 @@ TODO:
 from __future__ import absolute_import, division, print_function, unicode_literals
 import sys
 
-__version__ = '0.0.35'
+__version__ = '0.0.34'
 
 GLOBAL_MODULES = [
     'util_arg',
@@ -110,7 +110,7 @@ if _DOELSE:
     from ubelt.util_stress import (find_nth_prime,)
     from ubelt.util_time import (Timer, Timerit, timestamp,)
     from ubelt.progiter import (ProgIter,)
-    
+
     # </AUTOGEN_INIT>
 
 del _DOELSE
