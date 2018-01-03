b82b57ffce4d2b9be51b0fc2679d3262f925f997
Jon Crall <erotemic@gmail.com>
2018-01-02 21:35:45 -0500

    added tests for chunks

---
+++ ubelt/tests/test_list.py
@@ -0,0 +1,30 @@
+import pytest
+import ubelt as ub
+
+def test_chunk_errors():
+    with pytest.raises(ValueError):
+        ub.chunks(range(9))
+
+    with pytest.raises(ValueError):
+        ub.chunks(range(9), chunksize=2, nchunks=2)
+
+    with pytest.raises(TypeError):
+        len(ub.chunks((_ for _ in range(2)), nchunks=2))
+
+
+def test_chunk_total():
+    gen = ub.chunks([], total=10, chunksize=4)
+    assert len(gen) == 3
+
+
+def test_chunk_len():
+    gen = ub.chunks([1] * 6, chunksize=3)
+    assert len(gen) == 2
+
+if __name__ == '__main__':
+    r"""
+    CommandLine:
+        pytest ubelt/tests/test_list.py
+    """
+    import xdoctest
+    xdoctest.doctest_module(__file__)
