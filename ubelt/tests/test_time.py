9732d315fab84ea0a8d61b6549bbe93360102191
Jon Crall <erotemic@gmail.com>
2018-01-07 13:58:40 -0500

    improved timeing best practices and revised the timerit messages

--- ubelt/tests/test_time.py
+++ ubelt/tests/test_time.py
@@ -4,6 +4,7 @@ import pytest
 import ubelt as ub
 import re
 
+
 def test_timer_nonewline():
     from xdoctest.utils import CaptureStdout
     with CaptureStdout() as cap:
@@ -16,6 +17,33 @@ def test_timer_nonewline():
 def test_timestamp():
     stamp = ub.timestamp()
     assert re.match('\d+-\d+-\d+T\d+\+\d+', stamp)
+
+
+def test_timerit_verbose():
+    from xdoctest.utils import CaptureStdout
+    with CaptureStdout() as cap:
+        ub.Timerit(3, label='foo', verbose=0).call(lambda: None)
+    assert cap.text == ''
+
+    with CaptureStdout() as cap:
+        ub.Timerit(3, label='foo', verbose=1).call(lambda: None)
+    assert cap.text.count('\n') == 2
+    assert cap.text.count('foo') == 1
+
+    with CaptureStdout() as cap:
+        ub.Timerit(3, label='foo', verbose=2).call(lambda: None)
+    assert cap.text.count('\n') == 3
+    assert cap.text.count('foo') == 2
+
+    with CaptureStdout() as cap:
+        ub.Timerit(3, label='foo', verbose=3).call(lambda: None)
+    assert cap.text.count('\n') == 4
+    assert cap.text.count('foo') == 2
+
+    with CaptureStdout() as cap:
+        ub.Timerit(3, label='foo', verbose=4).call(lambda: None)
+    assert cap.text.count('\n') == 4
+    assert cap.text.count('foo') == 2
 
 
 def test_timestamp_value_error():
