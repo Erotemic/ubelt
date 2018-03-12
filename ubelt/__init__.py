bbce4d5ee2d84052a4caf8b6ab934ebba7ba22f7
Jon Crall <jon.crall@kitware.com>
2018-03-11 21:56:14 -0400

    version bump

--- ubelt/__init__.py
+++ ubelt/__init__.py
@@ -27,7 +27,7 @@ TODO:
 from __future__ import absolute_import, division, print_function, unicode_literals
 import sys
 
-__version__ = '0.0.43'
+__version__ = '0.0.44'
 
 GLOBAL_MODULES = [
     'util_arg',
@@ -126,7 +126,7 @@ if _DOELSE:  # pragma: nobranch
     from ubelt.util_time import (Timer, Timerit, timestamp,)
     from ubelt.orderedset import (OrderedSet, oset,)
     from ubelt.progiter import (ProgIter,)
-    
+
     # </AUTOGEN_INIT>
 
 del _DOELSE
