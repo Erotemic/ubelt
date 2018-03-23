1495c891c7f6c1cd737e688b65849d12dc38ffe6
Jon Crall <jon.crall@kitware.com>
2018-03-23 15:02:57 -0400

    Fix issues with oset

--- ubelt/orderedset.py
+++ ubelt/orderedset.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 from six.moves import zip, range
+from six.moves import filterfalse
 import itertools as it
 import collections
 import weakref
@@ -72,10 +73,17 @@ class OrderedSet(collections.MutableSet):
         Example:
             >>> self = OrderedSet([1, 3, 2])
             >>> assert self == [1, 3, 2]
-            >>> assert self == [1, 3, 2]
+            >>> assert self == [1, 2, 3]
+            >>> assert self != [2, 3]
             >>> assert self == OrderedSet([3, 2, 1])
         """
-        return not self.isdisjoint(other)
+        if len(self) == len(other):
+            return set(self) == set(other)
+        return False
+
+    def isdisjoint(self, other):
+        # Hack because apparently the inherited disjoint is now working
+        return len(self.intersection(other)) == 0
 
     def __iter__(self):
         """
@@ -215,18 +223,41 @@ class OrderedSet(collections.MutableSet):
         self.discard(key)
         return key
 
-    @classmethod
-    def union(cls, *sets):
+    def union(self, *sets):
         """
-        CommandLine:
-            python -m ubelt.orderedset OrderedSet.union
+        Combines all unique items.
+        Each items order is defined by its first appearance.
 
         Example:
-            >>> print(OrderedSet.union([3, 1, 4, 1, 5], [1, 3], [2, 0]))
+            >>> self = OrderedSet.union([3, 1, 4, 1, 5], [1, 3], [2, 0])
+            >>> print(self)
             OrderedSet([3, 1, 4, 5, 2, 0])
+            >>> self.union([8, 9])
+            OrderedSet([3, 1, 4, 5, 2, 0, 8, 9])
+            >>> self | {10}
+            OrderedSet([3, 1, 4, 5, 2, 0, 10])
         """
-        lists_ = list(it.chain.from_iterable(list(s) for s in sets))
-        return cls(lists_)
+        cls = self.__class__ if isinstance(self, OrderedSet) else OrderedSet
+        containers = map(list, it.chain([self], sets))
+        items = it.chain.from_iterable(containers)
+        return cls(items)
+
+    def intersection(self, *sets):
+        """
+        Returns elements in common between all sets. Order is defined only
+        by the first set.
+
+        Example:
+            >>> self = OrderedSet.intersection([0, 1, 2, 3], [1, 2, 3])
+            >>> print(self)
+            OrderedSet([1, 2, 3])
+            >>> self.intersection([2, 4, 5], [1, 2, 3, 4])
+            OrderedSet([2])
+        """
+        cls = self.__class__ if isinstance(self, OrderedSet) else OrderedSet
+        common = set.intersection(*map(set, sets))
+        items = (item for item in self if item in common)
+        return cls(items)
 
     def update(self, other):
         """
@@ -261,6 +292,125 @@ class OrderedSet(collections.MutableSet):
             if item == other:
                 return count
         raise ValueError('%r is not in OrderedSet' % (item,))
+
+    def copy(self):
+        """
+        Return a shallow copy of the ordered set.
+
+        Example:
+            >>> self = OrderedSet([1, 2, 3])
+            >>> other = self.copy()
+            >>> assert self == other and self is not other
+        """
+        return self.__class__(self)
+
+    def difference(self, *sets):
+        """
+        Returns all elements that are in this set but not the others.
+
+        Example:
+            >>> OrderedSet([1, 2, 3]).difference(OrderedSet([2]))
+            OrderedSet([1, 3])
+            >>> OrderedSet([1, 2, 3]) - OrderedSet([2])
+            OrderedSet([1, 3])
+        """
+        cls = self.__class__
+        other = set.intersection(*map(set, sets))
+        return cls(item for item in self if item not in other)
+
+    def issubset(self, other):
+        """
+        Report whether another set contains this set.
+
+        Example:
+            >>> OrderedSet([1, 2, 3]).issubset({1, 2})
+            False
+            >>> OrderedSet([1, 2, 3]).issubset({1, 2, 3, 4})
+            True
+            >>> OrderedSet([1, 2, 3]).issubset({1, 4, 3, 5})
+            False
+        """
+        if len(self) > len(other):  # Fast check for obvious cases
+            return False
+        return all(item in other for item in self)
+
+    def issuperset(self, other):
+        """
+        Report whether this set contains another set.
+
+        Example:
+            >>> OrderedSet([1, 2]).issuperset([1, 2, 3])
+            False
+            >>> OrderedSet([1, 2, 3, 4]).issuperset({1, 2, 3})
+            True
+            >>> OrderedSet([1, 4, 3, 5]).issuperset({1, 2, 3})
+            False
+        """
+        if len(self) < len(other):  # Fast check for obvious cases
+            return False
+        return all(item in self for item in other)
+
+    def symmetric_difference(self, other):
+        """
+        Return the symmetric difference of two sets as a new set.
+        (I.e. all elements that are in exactly one of the sets.)
+
+        Example:
+            >>> self = OrderedSet([1, 4, 3, 5, 7])
+            >>> other = OrderedSet([9, 7, 1, 3, 2])
+            >>> self.symmetric_difference(other)
+            OrderedSet([4, 5, 9, 2])
+        """
+        cls = self.__class__ if isinstance(self, OrderedSet) else OrderedSet
+        diff1 = cls(self).difference(other)
+        diff2 = cls(other).difference(self)
+        return diff1.union(diff2)
+
+    def difference_update(self, *sets):
+        """
+        Returns a copy of self with items from other removed
+
+        Example:
+            >>> self = OrderedSet([1, 2, 3])
+            >>> self.difference_update(OrderedSet([2]))
+            >>> print(self)
+            OrderedSet([1, 3])
+        """
+        for item in it.chain.from_iterable(sets):
+            self.discard(item)
+
+    def intersection_update(self, other):
+        """
+        Update a set with the intersection of itself and another.
+        Order depends only on the first element
+
+        Example:
+            >>> self = OrderedSet([1, 4, 3, 5, 7])
+            >>> other = OrderedSet([9, 7, 1, 3, 2])
+            >>> self.intersection_update(other)
+            >>> print(self)
+            OrderedSet([1, 3, 7])
+        """
+        to_remove = [item for item in self if item not in other]
+        for item in to_remove:
+            self.discard(item)
+
+    def symmetric_difference_update(self, other):
+        """
+        Update a set with the intersection of itself and another.
+        Order depends only on the first element
+
+        Example:
+            >>> self = OrderedSet([1, 4, 3, 5, 7])
+            >>> other = OrderedSet([9, 7, 1, 3, 2])
+            >>> self.symmetric_difference_update(other)
+            >>> print(self)
+            OrderedSet([4, 5, 9, 2])
+        """
+        cls = self.__class__ if isinstance(self, OrderedSet) else OrderedSet
+        diff2 = cls(other).difference(self)
+        self.difference_update(other)
+        self.update(diff2)
 
 
 # alias
