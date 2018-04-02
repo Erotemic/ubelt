e75d8373f590581c343cd5ee0de6bde873bba331
Jon Crall <jon.crall@kitware.com>
2018-04-02 15:22:44 -0400

    bumped to version 0.1.0

--- ubelt/__init__.py
+++ ubelt/__init__.py
@@ -19,7 +19,7 @@ TODO:
 from __future__ import absolute_import, division, print_function, unicode_literals
 import sys
 
-__version__ = '0.0.45.dev0'
+__version__ = '0.1.0'
 
 GLOBAL_MODULES = [
     'util_arg',
