6bf4825b8649cde7cca1e7aebf2755702f9b6347
joncrall <crallj@rpi.edu>
2018-04-20 00:57:01 -0400

    Added dzip

--- ubelt/util_dict.py
+++ ubelt/util_dict.py
@@ -99,6 +99,54 @@ class AutoOrderedDict(odict, AutoDict):
     _base = odict
 
 
+def dzip(items1, items2):
+    """
+    Zips elementwise pairs between items1 and items2 into a dictionary. Values
+    from items2 can be broadcast onto items1.
+
+    Args:
+        items1 (Sequence): full sequence
+        items2 (Sequence): can either be a sequence of one item or a sequence of
+            equal length to `items1`
+
+    Returns:
+        dict: similar to dict(zip(items1, items2))
+
+    Example:
+        >>> import pytest
+        >>> assert dzip([1, 2, 3], [4]) == {1: 4, 2: 4, 3: 4}
+        >>> assert dzip([1, 2, 3], [4, 4, 4]) == {1: 4, 2: 4, 3: 4}
+        >>> assert dzip([], [4]) == {}
+        >>> with pytest.raises(ValueError):
+        >>>     dzip([1, 2, 3], [])
+        >>> with pytest.raises(ValueError):
+        >>>     dzip([], [4, 5, 6])
+        >>> with pytest.raises(ValueError):
+        >>>     dzip([1, 2, 3], [4, 5])
+    """
+    try:
+        len(items1)
+    except TypeError:
+        items1 = list(items1)
+    try:
+        len(items2)
+    except TypeError:
+        items2 = list(items2)
+    if len(items1) == 0 and len(items2) == 1:
+        # Corner case:
+        # allow the first list to be empty and the second list to broadcast a
+        # value. This means that the equality check wont work for the case
+        # where items1 and items2 are supposed to correspond, but the length of
+        # items2 is 1.
+        items2 = []
+    if len(items2) == 1 and len(items1) > 1:
+        items2 = items2 * len(items1)
+    if len(items1) != len(items2):
+        raise ValueError('out of alignment len(items1)=%r, len(items2)=%r' % (
+            len(items1), len(items2)))
+    return dict(zip(items1, items2))
+
+
 def group_items(item_list, groupid_list, sorted_=True):
     r"""
     Groups a list of items by group id.
