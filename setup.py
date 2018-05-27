a93477835152c32e5651197f47651b0e9e64c3ff
joncrall <crallj@rpi.edu>
2018-05-27 16:01:27 -0400

    Using new mkinit, converted README to rst

--- setup.py
+++ setup.py
@@ -69,35 +69,18 @@ def parse_description():
     """
     Parse the description in the README file
 
+    pandoc --from=markdown --to=rst --output=README.rst README.md
+
     CommandLine:
         python -c "import setup; print(setup.parse_description())"
     """
     from os.path import dirname, join, exists
-    readme_fpath = join(dirname(__file__), 'README.md')
-    # print('readme_fpath = %r' % (readme_fpath,))
+    readme_fpath = join(dirname(__file__), 'README.rst')
     # This breaks on pip install, so check that it exists.
     if exists(readme_fpath):
-        # try:
-        #     # convert markdown to rst for pypi
-        #     import pypandoc
-        #     return pypandoc.convert(readme_fpath, 'rst')
-        # except Exception as ex:
-            # strip out markdown to make a clean readme for pypi
-            textlines = []
-            with open(readme_fpath, 'r') as f:
-                capture = False
-                for line in f.readlines():
-                    if '# Purpose' in line:
-                        capture = True
-                    elif line.startswith('##'):
-                        break
-                    elif capture:
-                        textlines += [line]
-            text = ''.join(textlines).strip()
-            text = text.replace('\n\n', '_NLHACK_')
-            text = text.replace('\n', ' ')
-            text = text.replace('_NLHACK_', '\n\n')
-            return text
+        with open(readme_fpath, 'r') as f:
+            text = f.read()
+        return text
     return ''
 
 
