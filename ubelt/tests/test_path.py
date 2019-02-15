ba82344e01d5f8bbd3b287030937969a36b04621
joncrall <erotemic@gmail.com>
2019-02-15 18:29:33 -0500

    working on 0.6.3

--- ubelt/tests/test_path.py
+++ ubelt/tests/test_path.py
@@ -2,6 +2,27 @@ from os.path import exists
 import ubelt as ub
 
 
+def test_pathlib():
+    try:
+        import pathlib
+        base = pathlib.Path(ub.ensure_app_cache_dir('ubelt'))
+        dpath = base.joinpath('test_pathlib_mkdir')
+
+        # ensuredir
+        ub.delete(dpath)
+        assert not dpath.exists()
+        got = ub.ensuredir(dpath)
+        assert got.exists()
+
+        # compressuser
+        assert ub.compressuser(base) == '~/.cache/ubelt'
+
+
+    except Exception:
+        import pytest
+        pytest.skip('pathlib is not installed')
+
+
 def test_tempdir():
     temp = ub.TempDir()
     assert temp.dpath is None
