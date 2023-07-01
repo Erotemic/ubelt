2c37957d39f8d39230f0123746625c8d38419b5d
joncrall <erotemic@gmail.com>
2023-07-01 14:23:46 -0400

    xcookie update

--- run_linter.sh
+++ run_linter.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-flake8 ./ubelt --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 --count --select=E9,F63,F7,F82 --show-source --statistics ubelt
+flake8 --count --select=E9,F63,F7,F82 --show-source --statistics ./tests
