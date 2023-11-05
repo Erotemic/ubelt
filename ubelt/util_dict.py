4014c0821c6e3e382e1cb78cbde301edf73047ea
joncrall <erotemic@gmail.com>
2023-11-04 20:08:28 -0400

    minor doc tweak

--- ubelt/util_dict.py
+++ ubelt/util_dict.py
@@ -1256,7 +1256,7 @@ class SetDict(dict):
     # We could just use the builtin variant for this specific operation
     def __or__(self, other):
         """
-        The | union operator
+        The ``|`` union operator
 
         Args:
             other (SupportsKeysAndGetItem[Any, Any] | Iterable[Tuple[Any, Any]]):
@@ -1268,7 +1268,7 @@ class SetDict(dict):
 
     def __and__(self, other):
         """
-        The & intersection operator
+        The ``&`` intersection operator
 
         Args:
             other (Mapping | Iterable):
@@ -1280,7 +1280,7 @@ class SetDict(dict):
 
     def __sub__(self, other):
         """
-        The - difference operator
+        The ``-`` difference operator
 
         Args:
             other (Mapping | Iterable):
@@ -1292,7 +1292,7 @@ class SetDict(dict):
 
     def __xor__(self, other):
         """
-        The ^ symmetric_difference operator
+        The ``^`` symmetric_difference operator
 
         Args:
             other (Mapping):
@@ -1375,7 +1375,6 @@ class SetDict(dict):
 
     def __rxor__(self, other):
         """
-
         Args:
             other (Mapping):
 
